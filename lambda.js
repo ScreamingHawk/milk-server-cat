3eea37751bcbceee60d26e6f17f06db211509d06
Michael Standen <screaminghawk@gmail.com>
2015-12-01 13:11:26 +1300

    Added cat facts email subscriptions and sending service

---
+++ lambda.js
@@ -0,0 +1,23 @@
+var AWS = require("aws-sdk");
+AWS.config.update({region: 'us-east-1'});
+
+exports.handler = function(event, context) {
+    var s3 = new AWS.S3();
+    var params = {Bucket: 'cat-assets', Key: 'catfacts.txt'};
+    var file = s3.getObject(params, function(err, data){
+        if(err){
+            context.fail(err);
+        }
+        var lines = data.Body.toString().split('\n');
+        var fact = lines[Math.floor(Math.random()*lines.length)] + "! :3";
+        console.log(fact);
+        
+        var sns = new AWS.SNS();
+        params = {
+            Message: fact,
+            Subject: "Cat Facts",
+            TopicArn: "arn:aws:sns:us-east-1:110820207274:CatFacts"
+        };
+        sns.publish(params, context.done);
+    });
+};
